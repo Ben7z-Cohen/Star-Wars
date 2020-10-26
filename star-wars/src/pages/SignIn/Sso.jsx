7659930fd4bfae80ced530b97d8c57e5b3c73a1d
ben7z <ben.cohen3@mail.huji.ac.il>
2020-10-26 20:44:56 +0200

    another className

--- star-wars/src/pages/SignIn/Sso.jsx
+++ star-wars/src/pages/SignIn/Sso.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Sso = ({ url, name, icon }) => {
     return (
-        <a class="no-underline near-white bg-animate bg-near-black 
+        <a className="no-underline near-white bg-animate bg-near-black 
         hover-bg-gray inline-flex items-center ma2 tc br2 pa2 w-100 pointer"
             title={name} onClick={url}>
             <img src={icon} alt={name} class="dib h2 w2" />
-            <span class="f5 ml3 pr2"> Sign in with {name} </span>
+            <span className="f5 ml3 pr2"> Sign in with {name} </span>
         </a>
     )
 }
